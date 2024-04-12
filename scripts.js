240c1211e001796dd9968ded8b46975fb8050c4f
LatyshevM <139449803+LatyshevM@users.noreply.github.com>
2024-04-12 16:58:28 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// Плавная прокрутка к якорям
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Дополнительные скрипты по мере необходимости
